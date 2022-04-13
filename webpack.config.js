23c8e8ed5c1efbf27b93f23f25671ee0e3ddfaab
Daniil Kazachkin <dreykanbern@icloud.com>
2022-04-13 14:09:59 +0300

    Pug

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
   mode: mode,
   output: {
     assetModuleFilename: 'assets/[hash][ext][query]',
+    clean: true,
   },
   devServer: {
     open: true,
@@ -58,6 +59,10 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|jpeg|svg|gif|webp)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
     ],
